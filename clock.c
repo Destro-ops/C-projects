7c969a075c17078a056dfca056508a236fe53241
Adarsh Yerneni <adarshyerneni@gmail.com>
2024-12-18 23:20:57 +0530

    final commit

--- clock.c
+++ clock.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
 #include<time.h>
 int main(){
+    struct tm *current_time;
+    char buffertime[50];
     time_t rawtime;
     time(&rawtime);
     char *string=ctime(&rawtime);
-    printf("%s",string);
+    current_time=localtime(&rawtime);
+    strftime(buffertime,sizeof(buffertime),"%I:%M:%S",current_time);
+    printf("%s",buffertime);
 
 }
